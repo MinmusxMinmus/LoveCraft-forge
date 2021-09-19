13d53976dd24c2fc3de5a28e9ef22d46ab668207
Min <minmusxminmus@hotmail.com>
2021-09-19 19:11:41 +0200

    Created registry for madness paths

--- src/main/java/com/minmusxminmus/lovecraft/LoveCraft.java
+++ src/main/java/com/minmusxminmus/lovecraft/LoveCraft.java
@@ -2,16 +2,22 @@ package com.minmusxminmus.lovecraft;
 
 import com.minmusxminmus.lovecraft.content.collections.Blocks;
 import com.minmusxminmus.lovecraft.content.collections.Items;
+import com.minmusxminmus.lovecraft.content.madness.paths.*;
 import com.minmusxminmus.lovecraft.proxy.Proxies;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.common.config.Config;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.registries.RegistryBuilder;
 import org.apache.logging.log4j.Logger;
+
 
 @Mod(modid = LoveCraft.MOD_ID, name = LoveCraft.NAME, version = LoveCraft.VERSION)
 @Mod.EventBusSubscriber
@@ -35,7 +41,18 @@ public class LoveCraft
     }
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event) { }
+    public void init(FMLInitializationEvent event) {
+        logger.info("Caching Madness path registry");
+        MadnessPath.MADNESS_REGISTRY = GameRegistry.findRegistry(MadnessPath.class);
+    }
+
+    @SubscribeEvent
+    public static void registerRegistries(RegistryEvent.NewRegistry event) {
+        RegistryBuilder<MadnessPath> registryBuilder = new RegistryBuilder<>();
+        registryBuilder.setType(MadnessPath.class);
+        registryBuilder.setName(new ResourceLocation(LoveCraft.MOD_ID, "madnesspaths"));
+        registryBuilder.create();
+    }
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event) {
@@ -55,6 +72,20 @@ public class LoveCraft
             logger.info("Registering block item \"" + block.getItem().getRegistryName() + "\"");
             event.getRegistry().register(block.getItem());
         });
+    }
+
+    @SubscribeEvent
+    public static void registerMadnessPaths(RegistryEvent.Register<MadnessPath> event) {
+        MadnessPath pathScience = new MadnessPathScience();
+        MadnessPath pathLore = new MadnessPathLore();
+        MadnessPath pathDeep = new MadnessPathDeep();
+        MadnessPath pathDream = new MadnessPathDream();
+        event.getRegistry().registerAll(
+                new MadnessPathScience().setRegistryName(new ResourceLocation(LoveCraft.MOD_ID, pathScience.getIdentifier())),
+                new MadnessPathLore().setRegistryName(new ResourceLocation(LoveCraft.MOD_ID, pathLore.getIdentifier())),
+                new MadnessPathDeep().setRegistryName(new ResourceLocation(LoveCraft.MOD_ID, pathDeep.getIdentifier())),
+                new MadnessPathDream().setRegistryName(new ResourceLocation(LoveCraft.MOD_ID, pathDream.getIdentifier()))
+        );
     }
 
     @SubscribeEvent
@@ -94,6 +125,34 @@ public class LoveCraft
             })
             @Config.RangeDouble(min = 0.0D)
             public static double MIN_MADNESS = 0.0D;
+
+            @Config.Name("Deep path enabled")
+            @Config.Comment({
+                    "This determines whether or not the Deep path is enabled.",
+                    "Disabling this means the player won't be able to enter said path, potentially locking them from certain features without the use of cheats."
+            })
+            public static boolean DEEP_PATH_ENABLED = true;
+
+            @Config.Name("Dream path enabled")
+            @Config.Comment({
+                    "This determines whether or not the Dream path is enabled.",
+                    "Disabling this means the player won't be able to enter said path, potentially locking them from certain features without the use of cheats."
+            })
+            public static boolean DREAM_PATH_ENABLED = true;
+
+            @Config.Name("Science path enabled")
+            @Config.Comment({
+                    "This determines whether or not the Science path is enabled.",
+                    "Disabling this means the player won't be able to enter said path, potentially locking them from certain features without the use of cheats."
+            })
+            public static boolean SCIENCE_PATH_ENABLED = true;
+
+            @Config.Name("Knowledge path enabled")
+            @Config.Comment({
+                    "This determines whether or not the Knowledge path is enabled.",
+                    "Disabling this means the player won't be able to enter said path, potentially locking them from certain features without the use of cheats."
+            })
+            public static boolean KNOWLEDGE_PATH_ENABLED = true;
         }
 
         @Config.Name("Madness configuration")
--- src/main/java/com/minmusxminmus/lovecraft/content/madness/paths/MadnessPath.java
+++ src/main/java/com/minmusxminmus/lovecraft/content/madness/paths/MadnessPath.java
@@ -1,18 +1,29 @@
 package com.minmusxminmus.lovecraft.content.madness.paths;
 
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 
-public abstract class MadnessPath {
+public abstract class MadnessPath extends IForgeRegistryEntry.Impl<MadnessPath> {
+    public static IForgeRegistry<MadnessPath> MADNESS_REGISTRY = null;
     public abstract String getIdentifier();
 
     /**
      * Convenience method to retrieve LoveCraft's default paths.
      */
-    public Collection<MadnessPath> getDefaultPaths() {
+    public static Collection<MadnessPath> getDefaultPaths() {
         Collection<MadnessPath> paths = new HashSet<>();
         Collections.addAll(paths, new MadnessPathDeep(), new MadnessPathDream(), new MadnessPathLore(), new MadnessPathScience());
         return paths;
     }
+
+    public static Collection<MadnessPath> getAllPaths() {
+        if (MADNESS_REGISTRY == null) return null;
+        return MADNESS_REGISTRY.getValuesCollection();
+    }
+
+
 }
