6f9cc2bc5f0ddcb26014575872d7da31f757a4f8
Min <minmusxminmus@hotmail.com>
2021-09-24 10:36:43 +0200

    Add command alias

--- src/main/java/com/minmusxminmus/lovecraft/content/commands/CommandLoveCraft.java
+++ src/main/java/com/minmusxminmus/lovecraft/content/commands/CommandLoveCraft.java
@@ -5,11 +5,25 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class CommandLoveCraft extends CommandBase {
     @Override
     public String getName() {
         return "LoveCraft";
     }
+
+    @Override
+    public List<String> getAliases() {
+        List<String> aliases = new ArrayList<>();
+        Collections.addAll(aliases, "lovecraft");
+        return aliases;
+    }
+
+
 
     @Override
     public String getUsage(ICommandSender sender) {
